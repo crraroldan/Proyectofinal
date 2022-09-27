c174460b0cb69d849bfd1bb4815cedc3c824b64a
cristian <crs1996ra@gmail.com>
2022-09-28 01:34:40 +0200

    Arreglo del funcionamiento de la cantidad y el reseteo

--- js/2main.js
+++ js/2main.js
@@ -1,150 +1,243 @@
-let totalpeliculas = document.getElementById("totalpeliculas");
-let cosa = 0;
+window.addEventListener("load",mostrar1);
+function mostrar1() {
+  var x = document.getElementById('Blade Runner');
+  if (x.style.display === 'none') {
+      x.style.display = 'block';
+  } else {
+     x.style.display  = 'none';
+  } 
+  
+}
 
-totalpeliculas.innerHTML = `${cosa} €`;
+window.addEventListener("load",mostrar2);
+function mostrar2() {
+  var x = document.getElementById('Blade Runner 2049');
+  if (x.style.display === 'none') {
+      x.style.display = 'block';
+  } else {
+      x.style.display = 'none';
+  }
 
-//Declaramos el objeto
+}
 
-let objetos = [
-    {
-        id: "Blade Runner",
-        precio: 1
-        
-    },
-    {
-        id: "Blade Runner 2049",
-        precio: 1
-        
-    },
-    {
-        id: "El señor de los anilos El retorno del rey",
-        precio: 1
-       
-    },
-    {
-        id: "El señor de los anilos La comunidad del anillo",
-        precio: 1
-       
-    },
-    {
-        id: "El señor de los anilos Las dos torres",
-        precio: 1
-       
-    },
-    {
-        id: "Starship Troopers",
-        precio: 1
-       
-    },
-    {
-        id: "Apocalypse Now",
-        precio: 1
-       
-    },
+window.addEventListener("load",mostrar3);
+function mostrar3() {
+  var x = document.getElementById('El señor de los anilos El retorno del rey');
+  if (x.style.display === 'none') {
+      x.style.display = 'block';
+  } else {
+      x.style.display = 'none';
+  }
 
-    {
-        id: "Robocop 1",
-        precio: 1
-        
-    },
-    {
-        id: "Robocop 2",
-        precio: 1
-        
-    },
-    {
-        id: "Robocop 3",
-        precio: 1
-       
-    },
-    {
-        id: "Soldado anonimo",
-        precio: 1
-       
-    },
-    {
-        id: "Independence Day",
-        precio: 1
-       
-    },
-    {
-        id: "Bad Boys 1",
-        precio: 1
-       
-    },
-    {
-        id: "Bad Boys 2",
-        precio: 1
-       
-    },
+}
+
+window.addEventListener("load",mostrar4);
+function mostrar4() {
+  var x = document.getElementById('El señor de los anilos La comunidad del anillo');
+  if (x.style.display === 'none') {
+      x.style.display = 'block';
+  } else {
+      x.style.display = 'none';
+  }
+
+}
 
 
-    {
-        id: "300",
-        precio: 1
-        
-    },
-    {
-        id: "Apocalypto",
-        precio: 1
-        
-    },
-    {
-        id: "Centurion",
-        precio: 1
-       
-    },
-    {
-        id: "Dune",
-        precio: 1
-       
-    },
-    {
-        id: "Sin tregua",
-        precio: 1
-       
-    },
-    {
-        id: "El Chema",
-        precio: 1
-       
-    },
-    {
-        id: "El Vato",
-        precio: 1
-       
-    }
+window.addEventListener("load",mostrar5);
+function mostrar5() {
+  var x = document.getElementById('El señor de los anilos Las dos torres');
+  if (x.style.display === 'none') {
+      x.style.display = 'block';
+  } else {
+      x.style.display = 'none';
+  }
 
-
-];
+}
 
 
 
-const drag = (ev) => {
-    ev.dataTransfer.setData("text", ev.target.id);
+window.addEventListener("load",mostrar6);
+function mostrar6() {
+  var x = document.getElementById('Starship Troopers');
+  if (x.style.display === 'none') {
+      x.style.display = 'block';
+  } else {
+      x.style.display = 'none';
+  }
 
-    // console.log("arrastrando...", ev.target.id);
-};
-
-const allowDrop = (ev) => {
-    ev.preventDefault();
-};
-
-const drop = (ev) => {
-    ev.preventDefault();
-    let data = ev.dataTransfer.getData("text");
-    // ev.target.appendChild(document.getElementById(data));
-    
-    let objetoDeseo = objetos.find(objeto => {
-        return objeto.id == data
-    });
-
-    
-    cosa += objetoDeseo.precio;
-    
-    totalpeliculas.innerHTML = `${cosa} €`;
+}
 
 
-}; 
 
+window.addEventListener("load",mostrar7);
+function mostrar7() {
+  var x = document.getElementById('Apocalypse Now');
+  if (x.style.display === 'none') {
+      x.style.display = 'block';
+  } else {
+      x.style.display = 'none';
+  }
+
+}
+
+
+window.addEventListener("load",mostrar8);
+function mostrar8() {
+  var x = document.getElementById('Robocop 1');
+  if (x.style.display === 'none') {
+      x.style.display = 'block';
+  } else {
+      x.style.display = 'none';
+  }
+
+}
+
+
+window.addEventListener("load",mostrar9);
+function mostrar9() {
+  var x = document.getElementById('Robocop 2');
+  if (x.style.display === 'none') {
+      x.style.display = 'block';
+  } else {
+      x.style.display = 'none';
+  }
+
+}
+
+
+
+window.addEventListener("load",mostrar10);
+function mostrar10() {
+  var x = document.getElementById('Robocop 3');
+  if (x.style.display === 'none') {
+      x.style.display = 'block';
+  } else {
+      x.style.display = 'none';
+  }
+
+}
+
+
+
+window.addEventListener("load",mostrar11);
+function mostrar11() {
+  var x = document.getElementById('Soldado anonimo');
+  if (x.style.display === 'none') {
+      x.style.display = 'block';
+  } else {
+      x.style.display = 'none';
+  }
+
+}
+
+window.addEventListener("load",mostrar12);
+function mostrar12() {
+  var x = document.getElementById('Independence Day');
+  if (x.style.display === 'none') {
+      x.style.display = 'block';
+  } else {
+      x.style.display = 'none';
+  }
+
+}
+
+window.addEventListener("load",mostrar13);
+function mostrar13() {
+  var x = document.getElementById('Bad Boys 1');
+  if (x.style.display === 'none') {
+      x.style.display = 'block';
+  } else {
+      x.style.display = 'none';
+  }
+
+}
+
+window.addEventListener("load",mostrar14);
+function mostrar14() {
+  var x = document.getElementById('Bad Boys 2');
+  if (x.style.display === 'none') {
+      x.style.display = 'block';
+  } else {
+      x.style.display = 'none';
+  }
+
+}
+
+window.addEventListener("load",mostrar15);
+function mostrar15() {
+  var x = document.getElementById('300');
+  if (x.style.display === 'none') {
+      x.style.display = 'block';
+  } else {
+      x.style.display = 'none';
+  }
+
+}
+
+window.addEventListener("load",mostrar16);
+function mostrar16() {
+  var x = document.getElementById('Apocalypto');
+  if (x.style.display === 'none') {
+      x.style.display = 'block';
+  } else {
+      x.style.display = 'none';
+  }
+
+}
+
+window.addEventListener("load",mostrar17);
+function mostrar17() {
+  var x = document.getElementById('Centurion');
+  if (x.style.display === 'none') {
+      x.style.display = 'block';
+  } else {
+      x.style.display = 'none';
+  }
+
+}
+
+window.addEventListener("load",mostrar18);
+function mostrar18() {
+  var x = document.getElementById('Dune');
+  if (x.style.display === 'none') {
+      x.style.display = 'block';
+  } else {
+      x.style.display = 'none';
+  }
+
+}
+
+window.addEventListener("load",mostrar19);
+function mostrar19() {
+  var x = document.getElementById('Sin tregua');
+  if (x.style.display === 'none') {
+      x.style.display = 'block';
+  } else {
+      x.style.display = 'none';
+  }
+
+}
+
+window.addEventListener("load",mostrar20);
+function mostrar20() {
+  var x = document.getElementById('The Batman');
+  if (x.style.display === 'none') {
+      x.style.display = 'block';
+  } else {
+      x.style.display = 'none';
+  }
+
+}
+
+
+window.addEventListener("load",mostrar21);
+function mostrar21() {
+  var x = document.getElementById('Scarface');
+  if (x.style.display === 'none') {
+      x.style.display = 'block';
+  } else {
+      x.style.display = 'none';
+  }
+
+}
 
--- js/main.js
+++ js/main.js
@@ -4,116 +4,125 @@ let precioTotal = 0;
 totalCarrito.innerHTML = `${precioTotal} €`;
 
 
+
+
+let totalpeliculas = document.getElementById("totalpeliculas");
+let Cantida = 0;
+
+totalpeliculas.innerHTML = `${Cantida}  uds`;
+
+
+
 //Declaramos el objeto
 
 let objetos = [
     {
         id: "Blade Runner",
-        precio: 8
-        
+        precio: 8,
+        Cantida: 1
     },
     {
         id: "Blade Runner 2049",
-        precio: 5
-        
+        precio: 9,
+        Cantida: 1
     },
     {
         id: "El señor de los anilos El retorno del rey",
-        precio: 4
-       
+        precio: 5,
+        Cantida: 1
     },
     {
         id: "El señor de los anilos La comunidad del anillo",
-        precio: 4
-       
+        precio: 5,
+        Cantida: 1
     },
     {
         id: "El señor de los anilos Las dos torres",
-        precio: 4
-       
+        precio: 5,
+        Cantida: 1
     },
     {
         id: "Starship Troopers",
-        precio: 4
-       
+        precio: 8,
+        Cantida: 1
     },
     {
         id: "Apocalypse Now",
-        precio: 4
-       
+        precio: 8,
+        Cantida: 1
     },
 
     {
         id: "Robocop 1",
-        precio: 8
-        
+        precio: 8,
+        Cantida: 1
     },
     {
         id: "Robocop 2",
-        precio: 5
-        
+        precio: 5,
+        Cantida: 1
     },
     {
         id: "Robocop 3",
-        precio: 8
-       
+        precio: 8,
+        Cantida: 1
     },
     {
         id: "Soldado anonimo",
-        precio: 7
-       
+        precio: 7,
+        Cantida: 1
     },
     {
         id: "Independence Day",
-        precio: 4
-       
+        precio: 8,
+        Cantida: 1
     },
     {
         id: "Bad Boys 1",
-        precio: 6
-       
+        precio: 6,
+        Cantida: 1
     },
     {
         id: "Bad Boys 2",
-        precio: 8
-       
+        precio: 8,
+        Cantida: 1
     },
 
 
     {
         id: "300",
-        precio: 8
-        
+        precio: 8,
+        Cantida: 1
     },
     {
         id: "Apocalypto",
-        precio: 7
-        
+        precio: 7,
+        Cantida: 1
     },
     {
         id: "Centurion",
-        precio: 8
-       
+        precio: 8,
+        Cantida: 1
     },
     {
         id: "Dune",
-        precio: 10
-       
+        precio: 10,
+        Cantida: 1
     },
     {
         id: "Sin tregua",
-        precio: 6
-       
+        precio: 6,
+        Cantida: 1
     },
     {
-        id: "El Chema",
-        precio: 7
-       
+        id: "The Batman",
+        precio: 7,
+        Cantida: 1
     },
     {
-        id: "El Vato",
-        precio: 8
-       
+        id: "Scarface",
+        precio: 8,
+        Cantida: 1
     }
 
 
@@ -144,9 +153,52 @@ const drop = (ev) => {
     precioTotal += objetoDeseo.precio;
     
     totalCarrito.innerHTML = `${precioTotal} €`;
+  //    -------------------------------------------------------------------
+
+    let objetocantidad = objetos.find(objeto => {
+        return objeto.id == data
+    });
+
+    
+    Cantida += objetocantidad.Cantida;
+    
+    totalpeliculas.innerHTML = `${Cantida} uds`;
+
 
 
 }; 
+
+
+
+
+
+
+
+
+
+let variableGlobal = 0;
+let carrito = document.getElementById("totalCarrito");
+
+const vaciar = () => {
+    variableGlobal = 0;
+
+    carrito.innerHTML = `${variableGlobal}`;
+}
+
+
+let muchos = document.getElementById("totalpeliculas");
+
+const vaciar2 = () => {
+     variableGlobal = 0;
+
+   muchos.innerHTML = `${variableGlobal}`;
+}
+
+
+
+
+
+
 
 
 
